ba3d5a5f4bd3373bc9ccac387c201a60588e06f2
John Mallavalli <jmallavalli@outlook.com>
2023-11-01 14:35:12 +1000

    Added Internationalization

--- src/main/java/com/jmallavalli/springboot/restapi/controller/GreetingController.java
+++ src/main/java/com/jmallavalli/springboot/restapi/controller/GreetingController.java
@@ -1,15 +1,32 @@
 package com.jmallavalli.springboot.restapi.controller;
 
 import com.jmallavalli.springboot.restapi.model.GreetBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class GreetingController {
+
+    private final MessageSource messageSource;
+
+    public GreetingController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello")
     public String greet() {
         return "Hello and welcome to the world of Spring Boot!";
+    }
+
+    @GetMapping(path = "/hellov2")
+    public String greetI18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("greeting.msg", null, "Default Greeting Message", locale);
     }
 
     @GetMapping(path = "/helloBean")
